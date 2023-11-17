459bcd62dafafe2641eabfe2c92838db7b49023f
Koushik Jhade <81366544+IdleJay@users.noreply.github.com>
2023-11-17 22:18:35 +0530

    update!

---
+++ JS/Looping/for-loop.js
@@ -0,0 +1,23 @@
+/* for (var i = 0; i <= 100; i++) {
+    console.log(i);
+}
+console.log('Count Complete!'); */
+
+/* for (var i = 60; i >= 0; i--) {
+    console.log(i);
+}
+console.log('Happy New Year!') */
+
+/* var i = 200;
+while (i <= 500) {
+    console.log(i);
+    i = i + 1;
+}
+console.log('Maximum Speed Reached!'); */
+
+/* var i = 365;
+while (i >= 1) {
+    console.log(i);
+    i = i - 1;
+}
+console.log('The year is over!'); */
