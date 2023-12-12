4304e8cd5662c1a04762399092e95782c77f37e8
Koushik Jhade <81366544+IdleJay@users.noreply.github.com>
2023-12-12 09:47:41 +0530

    Create template_literals.js

---
+++ JS/template_literals.js
@@ -0,0 +1,11 @@
+/*
+let temp = 23
+let place = 'Edinburgh'
+
+// console.log("Today it's " + temp + " degrees C here in " + place)
+//Template literals- instead of using the inverted commas, we'll use the back ticks
+console.log(`Today it's ${temp} degrees C here in ${place}`)
+*/
+
+let noMultiLine = "No multi-line strings in ESS";
+console.log("Did you know? " + noMultiLine);
