8318471fad5d84f109ef0be44ef9f5673538d7cd
Koushik Jhade <81366544+IdleJay@users.noreply.github.com>
2023-12-14 10:31:34 +0530

    jest-testing

---
+++ JS/learn/jest-testing/timesTwo.js
@@ -0,0 +1,5 @@
+function timesTwo(val) {
+    return val * 2;// Task 1: Code the timesTwo function declaration
+};
+
+module.exports = timesTwo;// Task 2: Export the timesTwo function as a module
---
+++ python/DSA_sesh1.py
@@ -0,0 +1,5 @@
+#Alice has some cards with numbers on them. She arranges the cards in decreasing
+#order, and lays them out face down in a sequence on a table. She challenges Bob
+#to pick out the card containing a given number by turning over as few cards as
+#possible. Write a function to help bob locate the card.
+
