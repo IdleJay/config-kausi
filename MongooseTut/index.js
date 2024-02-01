e4002f7fdb5bc0e4f972fd0b3ca76ebb7512218b
Koushik Jhade <koushik.jhade@gmail.com>
2024-02-01 09:33:42 +0530

    MoongooseConnect

--- MongooseTut/index.js
+++ MongooseTut/index.js
@@ -2,19 +2,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
-mongoose.connect('mongodb://localhost:27017/')
+mongoose.connect('mongodb://127.0.0.1:27017/jayKart')
 
 const UserSchema = mongoose.Schema({
     name: String,
-    age: Number
+    
 })
 
-const UserModel = mongoose.model("jayKart", UserSchema)
+const UserModel = mongoose.model("items", UserSchema)
 
 app.get("/getUsers", (req, res) => {
-    res.json(UserModel.find())
+    UserModel.find().then(function(items) {
+        res.json(items)
+    }).catch(function(err) {
+        console.log(err)
+    })
 })
 
 app.listen(3001, () => {
     console.log('Server is running')
 })
+
