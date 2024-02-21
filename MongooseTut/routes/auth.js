e09035080ed4fa26462c3006f2b4287561d6a2dc
Koushik Jhade <koushik.jhade@gmail.com>
2024-02-21 20:35:35 +0530

    backFromVacayUpdate

--- MongooseTut/routes/auth.js
+++
@@ -1,7 +0,0 @@
-// routes/auth.js
-const express = require('express');
-const router = express.Router();
-
-// Authentication routes
-
-module.exports = router;
--- MongooseTut/routes/profiles.js
+++
@@ -1,7 +0,0 @@
-// routes/profile.js
-const express = require('express');
-const router = express.Router();
-
-// Profile-related routes
-
-module.exports = router;
